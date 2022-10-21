b5c26c4f09a6d674672f4673ca8877b375294f8f
Thotino GOBIN-GANSOU <thotino@lalalab.com>
2022-10-21 09:12:46 +0200

    INIT: initial commit

---
+++ doublyLinkedList.js
@@ -0,0 +1,66 @@
+class DoublyLinkedList {
+    constructor() {
+        this.nodes = []
+    }
+
+    get size() {
+        return this.nodes.length
+    }
+
+    get head() {
+        return (this.size) ? this.nodes[0] : null
+    }
+
+    get tail() {
+        return (this.size) ? this.nodes[this.size - 1] : null
+    }
+
+    insertAt(idx, value) {
+        const previousNode = this.nodes[idx - 1] || null
+        const nextNode = this.nodes[idx] || null
+        const node = { value, next: nextNode, previous: previousNode }
+
+        if (previousNode) previousNode.next = node
+        if (nextNode) nextNode.previous = node
+        this.nodes.splice(idx, 0, node)
+    }
+
+    insertFirst(value) {
+        this.insertAt(0, value)
+    }
+
+    insertLast(value) {
+        this.insertAt(this.size, value)
+    }
+
+    getAt(idx) {
+        return this.nodes[idx]
+    }
+
+    removeAt(idx) {
+        const previousNode = this.nodes[idx - 1] || null
+        const nextNode = this.nodes[idx + 1] || null
+        if (previousNode) previousNode.next = nextNode        
+        if (nextNode) nextNode.previous = previousNode        
+        this.nodes.splice(idx, 1)
+    }
+
+    clear() {
+        this.nodes = []
+    }
+
+    reverse() {
+        this.nodes = this.nodes.reduce((acc, { value }) => {
+            const nextNode = acc[0] || null
+            const node = { value, next: nextNode, previous: null }
+            if (nextNode) nextNode.previous = node
+            return [node, ...acc]
+        }, [])
+    }
+
+    /* Generator method */
+    *[Symbol.iterator]() {
+        yield* this.nodes
+    }
+    
+}
